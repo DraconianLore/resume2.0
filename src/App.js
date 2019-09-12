9eb5dfcbb119685677d7aa5f4afeee8c138864e8
Steven Wing <steven.wing82@gmail.com>
2019-09-12 15:19:54 -0700

    set up nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navbar from './Navbar';
+import Resume from './resume/Resume';
+import Portfolio from './portfolio/Portfolio';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      activePage: 'portfolio'
+    }
+  }
+
+  setActive = (pageName) => {
+    this.setState({activePage: pageName})
+  }
+  render() {
+
+    return (
+      <div className="App">
+        <Navbar setActive={this.setActive} />
+        {this.state.activePage === 'portfolio' && <Portfolio />}
+        {this.state.activePage === 'resume' && <Resume />}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      portfolio: 'active-tab',
+      resume: 'inactive-tab'
+    }
+  }
+  setActive = (e) => {
+    e.preventDefault();
+    const clicked = e.target.id;
+    if(this.state[clicked] === 'inactive-tab') {
+      if(clicked === 'portfolio') {
+        this.setState({
+          portfolio: 'active-tab',
+          resume: 'inactive-tab'
+        })
+        this.props.setActive('portfolio')
+      } else {
+        this.setState({
+          portfolio: 'inactive-tab',
+          resume: 'active-tab'
+        })
+        this.props.setActive('resume')
+      }
+    } 
+  }
+  render() {
+    return (
+      <div className="top-bar">
+        <button id='portfolio' className={this.state.portfolio} onClick={this.setActive}>
+          Portfolio
+        </button>
+        <button id='resume' className={this.state.resume} onClick={this.setActive}>
+          Resume
+        </button>
+      </div>
+
+    )
+  }
+}
