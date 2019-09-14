c621bb657298114b604f2d2d44cfe6eb8ea0bf85
Steven Wing <steven.wing82@gmail.com>
2019-09-13 23:37:49 -0700

    Updates

---
+++ precache-manifest.184f905ac2ffb28c83427d216979f6ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431f648863bc8b7ea625815cc7afb323",
+    "url": "/resume2.0/index.html"
+  },
+  {
+    "revision": "e8e73f05479a45c4f2e4",
+    "url": "/resume2.0/static/css/main.6e40483d.chunk.css"
+  },
+  {
+    "revision": "6eb28796552a25bb5690",
+    "url": "/resume2.0/static/js/2.1638c804.chunk.js"
+  },
+  {
+    "revision": "e8e73f05479a45c4f2e4",
+    "url": "/resume2.0/static/js/main.4c6e6f23.chunk.js"
+  },
+  {
+    "revision": "bcb7e0b2261bc3cf41a6",
+    "url": "/resume2.0/static/js/runtime~main.9663a35d.js"
+  }
+]);
