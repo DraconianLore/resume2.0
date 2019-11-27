4d440be24e74625a3bb777ebcdfceec00b92d10b
Steven Wing <steven.wing82@gmail.com>
2019-11-26 20:24:36 -0800

    added stigma

--- src/portfolio/projects/Current.js
+++ src/portfolio/projects/Current.js
@@ -11,17 +11,7 @@ export default class Current extends Component {
         
         <hr className='project-hr' />
         <h4>
-        <a className='port-link' href='https://github.com/rsmsnot/Sports-Guide' target='_blank' rel="noopener noreferrer">Sports Guide</a>
-         </h4>
-        <p>
-        <em>A Sports guide/fixture list application</em>
-        </p>
-        <p className='port-current'>
-          Working with a small team on this project, designed to display the times and which TV channel it will be showing on in your country.
-        </p>
-        <hr className='project-hr' />
-        <h4>
-          <a className='port-link' href='https://github.com/DraconianLore/dmPlayerTracker' target='_blank' rel="noopener noreferrer">DM Player Tracker</a>
+          <a className='port-link' href='https://draconianlore.github.io/dmPlayerTracker' target='_blank' rel="noopener noreferrer">DM Player Tracker</a>
          </h4>
         <p>
         <em>A Dungeons & Dragons Player tracker for Dungeon Masters</em>
@@ -30,15 +20,25 @@ export default class Current extends Component {
           My personal project, where as an avid D&D player, and regular Dungeon Master, I decided it would be nice to have an app to track player information for my players. Using React and Rails with a PostgreSQL database I am actively working on this project.
         </p>
         <hr className='project-hr' />
+        <h4>
+          <a className='port-link' href='http://www.stigma.ink' target='_blank' rel="noopener noreferrer">Stigma.ink</a>
+         </h4>
+        <p>
+          <em>A site for tattoo artist to manage client bookings and events</em>
+        </p>
+        <p className='port-current'>
+          A project I am collaborating on that allows tattoo artists to connect with clients to set up bookings, host events, and communicate directly with their clients. Using React and Rails with a PostgreSQL database and AWS hosting for images.
+        </p>
+        <hr className='project-hr' />
         <h1>Open Source Contributions</h1>
         <hr className='project-hr' />
         <h4>
-          <a className='port-link' href='https://github.com/publiclab/mapknitter' target='_blank' rel="noopener noreferrer">Mapknitter</a>
+          <a className='port-link' href='http://mapknitter.org' target='_blank' rel="noopener noreferrer">Mapknitter</a>
          </h4>
         <p>
           <em>An open source image(aerial) to map joining application</em>
         </p>
-
+      
         
       </div>
     )
